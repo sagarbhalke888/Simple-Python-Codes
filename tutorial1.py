8364848a1b1174492fa653a475a0bb616a600b8b
sagarbhalke888 <47658130+sagarbhalke888@users.noreply.github.com>
2019-04-27 01:17:21 +0530

    Create tutorial1.py

---
+++ tutorial1.py
@@ -0,0 +1,19 @@
+#Dealing with integers
+a=7
+b=8
+sum=a+b
+sub=a-b
+print(sum)
+print(sub)
+
+#dealing with strings
+
+s1="i love "
+s2="my country"
+s3='sagar'
+
+
+print(s1+s2)
+print('s3')
+print("enter the number")
+print("the number of suscribers on my channel are",a+b)
