46337fff2a1679fd2513875e9dacee615547bca1
sagarbhalke888 <47658130+sagarbhalke888@users.noreply.github.com>
2019-04-28 03:49:08 +0530

    Create input_print.py

---
+++ input_print.py
@@ -0,0 +1,11 @@
+print("my name is sagar")
+print("my name is raghu "
+      "i am from mumbai")
+print(''' ukgsbjm 
+ ,ugjvjvbgvn
+ o igh;alvkn,
+  kuefvjjtbnk''')
+print('''my name is raghu 
+      i am from                            mumbai''')
+a=input("Enter the number")
+print("you entered ",a)
