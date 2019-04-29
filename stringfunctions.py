4ff91fc60cb80d2a2965ee00e5e4601bb894003e
sagarbhalke888 <47658130+sagarbhalke888@users.noreply.github.com>
2019-04-29 21:20:45 +0530

    Create stringfunctions.py

---
+++ stringfunctions.py
@@ -0,0 +1,14 @@
+str="taj mahal is in india"
+a=str.capitalize()
+print(a)
+b=str.upper()
+print(b)
+print(str.islower())
+print(str.isupper())
+print(b.islower())
+# find
+print(str.find('mahal'))
+
+#replace()
+print(str.replace('mahal','hotel'))
+print(str.index('in'))
