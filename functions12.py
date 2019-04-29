b5e90dd80751e8617c22540d777d49333ae51a1c
sagarbhalke888 <47658130+sagarbhalke888@users.noreply.github.com>
2019-04-30 03:04:07 +0530

    Create functions12.py

---
+++ functions12.py
@@ -0,0 +1,20 @@
+def addition(a,b):
+    sum=a+b
+    print("The sum is",sum)
+
+def sub(a,b):
+    sub=a-b
+    return sub
+
+a=input("Enter first no.")
+a=int(a)
+b=input("Enter second no.")
+b=int(b)
+
+
+
+#addition(a,b)
+x = sub(a,b)
+
+print("the sub is ",x)
+
